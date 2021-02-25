9f041ebfd5e7dcc9e3afcc7d4d41d6da026dd6f0
Andrei Dergachev <andrei@dishtv.co.nz>
2021-02-25 19:10:56 +1300

    Impvovenments with calculations: - logic for evaluator#2

--- app/src/main/java/nz/calculatorpp/android/bigbuttoncalculator/Calculator.kt
+++ app/src/main/java/nz/calculatorpp/android/bigbuttoncalculator/Calculator.kt
@@ -10,103 +10,84 @@ object Calculator {
         )
     }
 
-    fun calc(expression: String): Double? {
-        var expression = expression
-
-        if (expression.startsWith("(") && expression.endsWith(")")) {
-            return calc(
-                expression.substring(1, expression.length - 1)
-            )
-        }
-        val containerArr = arrayOf(expression)
-        var leftVal =
-            getNextOperand(
-                containerArr
-            )
-        expression = containerArr[0]
-        if (expression.length == 0) {
-            return leftVal
-        }
-        var operator = expression[0]
-        expression = expression.substring(1)
-
-        while (operator == '×' || operator == '/' || operator == '%') {
-            containerArr[0] = expression
-            val rightVal =
-                getNextOperand(
-                    containerArr
-                )
-            expression = containerArr[0]
-            if (operator == '×') {
-                leftVal = leftVal * rightVal
-            } else if (operator == '/') {
-                leftVal = leftVal / rightVal
-            } else if (operator == '%') {
-                leftVal = leftVal / 100 * rightVal
+    fun calc(str: String): Double {
+        return object : Any() {
+            var pos = -1
+            var ch = 0
+            fun nextChar() {
+                ch = if (++pos < str.length) str[pos].toInt() else -1
             }
-            if (expression.length > 0) {
-                operator = expression[0]
-                expression = expression.substring(1)
-            } else {
-                return leftVal
-            }
-        }
-        return if (operator == '+') {
-            leftVal + calc(
-                expression
-            )!!
-        } else {
-            leftVal - calc(
-                expression
-            )!!
-        }
 
-
-    }
-
-    private fun getNextOperand(exp: Array<String>): Double {
-        val res: Double
-        if (exp[0].startsWith("(")) {
-            var open = 1
-            var i = 1
-            while (open != 0) {
-                if (exp[0][i] == '(') {
-                    open++
-                } else if (exp[0][i] == ')') {
-                    open--
+            fun eat(charToEat: Int): Boolean {
+                while (ch == ' '.toInt()) nextChar()
+                if (ch == charToEat) {
+                    nextChar()
+                    return true
                 }
-                i++
+                return false
             }
-            res = calc(
-                exp[0].substring(
-                    1,
-                    i - 1
-                )
-            )!!
-            exp[0] = exp[0].substring(i)
-        } else {
-            var i = 1
-            if (exp[0][0] == '-') {
-                i++
+
+            fun parse(): Double {
+                nextChar()
+                val x = parseExpression()
+                if (pos < str.length) throw RuntimeException("Unexpected: " + ch.toChar())
+                return x
             }
-            while (exp[0].length > i && isNumber(
-                    exp[0][i].toInt()
-                )
-            ) {
-                i++
+
+            // Grammar:
+            // expression = term | expression `+` term | expression `-` term
+            // term = factor | term `*` factor | term `/` factor
+            // factor = `+` factor | `-` factor | `(` expression `)`
+            //        | number | functionName factor | factor `^` factor
+            fun parseExpression(): Double {
+                var x = parseTerm()
+                while (true) {
+                    if (eat('+'.toInt())) x += parseTerm() // addition
+                    else if (eat('-'.toInt())) x -= parseTerm() // subtraction
+                    else return x
+                }
             }
-            res = java.lang.Double.parseDouble(exp[0].substring(0, i))
-            exp[0] = exp[0].substring(i)
-        }
-        return res
+
+            fun parseTerm(): Double {
+                var x = parseFactor()
+                while (true) {
+                    if (eat('×'.toInt())) x *= parseFactor() // multiplication
+                    else if (eat('/'.toInt())) x /= parseFactor() // division
+                    else return x
+                }
+            }
+
+            fun parseFactor(): Double {
+                if (eat('+'.toInt())) return parseFactor() // unary plus
+                if (eat('-'.toInt())) return -parseFactor() // unary minus
+                var x: Double
+                val startPos = pos
+                if (eat('('.toInt())) { // parentheses
+                    x = parseExpression()
+                    eat(')'.toInt())
+                } else if (ch >= '0'.toInt() && ch <= '9'.toInt() || ch == '.'.toInt()) { // numbers
+                    while (ch >= '0'.toInt() && ch <= '9'.toInt() || ch == '.'.toInt()) nextChar()
+                    x = str.substring(startPos, pos).toDouble()
+                } else if (ch >= 'a'.toInt() && ch <= 'z'.toInt()) { // functions
+                    while (ch >= 'a'.toInt() && ch <= 'z'.toInt()) nextChar()
+                    val func = str.substring(startPos, pos)
+                    x = parseFactor()
+                    x =
+                        if (func == "sqrt") Math.sqrt(x) else if (func == "sin") Math.sin(
+                            Math.toRadians(
+                                x
+                            )
+                        ) else if (func == "cos") Math.cos(Math.toRadians(x)) else if (func == "tan") Math.tan(
+                            Math.toRadians(x)
+                        ) else throw RuntimeException(
+                            "Unknown function: $func"
+                        )
+                } else {
+                    throw RuntimeException("Unexpected: " + ch.toChar())
+                }
+                if (eat('^'.toInt())) x = Math.pow(x, parseFactor()) // exponentiation
+                return x
+            }
+        }.parse()
     }
-
-
-    private fun isNumber(c: Int): Boolean {
-        val zero = '0'.toInt()
-        val nine = '9'.toInt()
-        return c >= zero && c <= nine || c == '.'.toInt()
-    }
-
-
 }
